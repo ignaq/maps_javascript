198dd4cc7e52c96794b88ffc655b2612c344d974
Juan Ignacio Quintero <j.ignacioquintero@gmail.com>
2021-09-07 10:13:19 -0300

    agrego una funcion que al hacer click en el mapa guarda las coordenadas en el formulario

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 window.onload = function cargaMap() {
-
     let myPosition = new google.maps.LatLng(-34.602209, -58.376626);
   
     let option = {
@@ -9,4 +8,16 @@ window.onload = function cargaMap() {
     };
   
     map = new google.maps.Map(document.getElementById('map'), option);
+
+    map.addListener('click', function(event){
+  
+    document.getElementById("coords").value = event.latLng.lat() + ", " + event.latLng.lng();
+    
+      let myModal = new bootstrap.Modal(document.getElementById('modalId'))
+        myModal.show()
+
+    })
+
+
 }
+
